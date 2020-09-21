3251a70c9761bbbd8537879a3cb689a82850acc7
Bryan Erazo <br@MacBook-Pro.local>
2020-09-21 18:24:50 -0400

    Added LinkList data structure, print linkList function and some test cases in the main

--- main.c
+++ main.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+//  Data Structure LinkList Node
+struct Node{
+    int data;
+    struct Node* next;
+};
+
+// Print the LinkList
+void printList(struct Node* node)
+{
+    if (node == NULL){
+        printf("EMPTY\n");
+    }
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+}
+
 
 int main() {
-    printf("Hello, World!\n");
+    struct Node* head = NULL;
+    struct Node* node = NULL;
+
+    head = (struct Node*)malloc(sizeof(struct Node));
+    node = (struct Node*)malloc(sizeof(struct Node));
+
+    head->data = 3;
+    head->next = node;
+
+    node->data = 5;
+
+    printList(head);
+    printf("\n");
+    printList(node);
+
+
+
+
     return 0;
 }
