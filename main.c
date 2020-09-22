948e942ab416a24767d16ae608a24a5ebe54949a
Bryan F. Erazo <bfe4@ilab3.cs.rutgers.edu>
2020-09-22 01:33:50 -0400

    Memory leak bug fixed

--- main.c
+++ main.c
@@ -49,6 +49,7 @@ void insertNode( struct Node** node, int location, int data ){
 
     // This is use for skip insertion
     if ( !newNode || location == 0){
+	free(newNode);    
         return;
     }
 
